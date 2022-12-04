b686fde1817166907df15f50c9410b3ce806a739
shrektan <shrektan@126.com>
2022-12-05 02:33:34 +0800

    add simplify file tree script Quickly flat the file structure by removing the first level directories

---
+++ simplify-file-tree.py
@@ -0,0 +1,42 @@
+"""Simplify The File Tree
+Quickly flat the file structure by removing the first level directories
+"""
+import sys
+import os
+import shutil
+import subprocess
+
+target = ""
+if len(sys.argv) >= 2:
+    target = sys.argv[1].strip()
+if len(target) == 0:
+    target = input("Please enter the target folder:\n").strip()
+if len(target) == 0 or os.path.exists(target) is False or \
+   os.path.isdir(target) is False:
+    msg = f"You must provide a valid folder (current input is '{target}')"
+    raise FileExistsError(msg)
+
+for entry in os.scandir(target):
+    if entry.is_dir():
+        for f in os.scandir(entry.path):
+            file_name = f.name
+            if file_name[0] == ".":
+                continue
+            if file_name == entry.name:
+                file_name = "0-" + file_name
+            dest = os.path.join(target, file_name)
+            shutil.move(f.path, dest)
+
+if input("Clean the empty folder? [y]/n") != "n":
+    for entry in os.scandir(target):
+        if entry.is_dir():
+            is_empty = True
+            for f in os.scandir(entry.path):
+                if file_name[0] != ".":
+                    is_empty = False
+                    break
+            if is_empty:
+                os.removedirs(entry.path)
+
+print(f"{target=} cleaned.")
+subprocess.run(["open", target])
