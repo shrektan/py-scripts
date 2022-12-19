8fb6b0d2a24f0a4a9a6a44d7ef131af912935808
shrektan <shrektan@126.com>
2022-12-19 21:53:47 +0800

    add a simple web server

---
+++ simple_web_server.py
@@ -0,0 +1,30 @@
+"""一个无敌简单的web服务器
+"""
+import socket
+import os
+
+WEBROOT = "./webroot"
+
+def handle_client(c: socket.socket, addr: str):
+    print(f"{addr} connected")
+    with c:
+        request = c.recv(1024)
+        headers = request.split(b"\r\n")
+        file = headers[0].split()[1].decode()
+        if file == "/":
+            file = "/index.html"
+        try:
+            with open(WEBROOT + file, "rb") as f:
+                content = f.read()
+            response = b"HTTP/1.0 200 OK\r\n\r\n" + content
+        except FileNotFoundError:
+            response = b"HTTP/1.0 404 NOT FOUND\r\n\r\nFile not found!"
+        c.sendall(response)
+
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("0.0.0.0", 80))
+    s.listen()
+    while True:
+        c, addr = s.accept()
+        handle_client(c, addr)
