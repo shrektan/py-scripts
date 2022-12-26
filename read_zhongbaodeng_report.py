fa4309a9048b26cd0ae4ec9123b825665acd22f1
shrektan <shrektan@126.com>
2022-12-27 01:22:44 +0800

    support mmp and fix type errors

--- read_zhongbaodeng_report.py
+++ read_zhongbaodeng_report.py
@@ -10,11 +10,11 @@ pd.set_option('display.unicode.east_asian_width',True)
 
 
 def rate2num(x :pd.Series) -> list | pd.Series:
-    if x is not pd.Series[str]:
+    if not pd.api.types.is_string_dtype(x):
         return x
     out :list = []
     for elem in x:
-        if elem == "-":
+        if pd.isna(elem) or elem == "-":
             out.append(pd.NA)
         elif elem.find("%") > -1:
             out.append(pd.to_numeric(elem.replace("%", "")) / 100.0)
@@ -23,12 +23,31 @@ def rate2num(x :pd.Series) -> list | pd.Series:
     return out
 
 
-def find_prod_type(x: str) -> str:
+def find_prod_type(x: list[str]) -> str:
+    txt = "".join(filter(lambda x: (n := x.count(" "), n != 6 and n != 4), x))
     opts = ["固定收益类", "混合类", "权益类", "货币类"]
     for opt in opts:
-        if x.count(opt) > 0:
+        if opt in txt:
             return opt
     return "N/A"
+
+
+def conv_normal(x: list[str]) -> pd.DataFrame:
+    txt = filter(lambda x: x.count(" ") == 6, x)
+    cols = [
+        "产品全称", "产品成立时间", "期末累计单位净值(元/份)",
+        "当月累计单位净值增长率", "年初以来累计单位净值增长率", "期末净资产(亿元)", "产品管理机构"
+    ]
+    return pd.DataFrame(map(lambda x: x.split(" "), txt), columns=cols)
+
+
+def conv_mmp(x: list[str]) -> pd.DataFrame:
+    txt = filter(lambda x: x.count(" ") == 4, x)
+    cols = ["产品全称", "产品成立时间", "年初以来累计单位净值增长率", "期末净资产(亿元)", "产品管理机构"]
+    out = pd.DataFrame(map(lambda x: x.split(" "), txt), columns=cols)
+    out["期末累计单位净值(元/份)"] = pd.NA
+    out["当月累计单位净值增长率"] = pd.NA
+    return out
 
 
 def read_tbl(pdf_path: pathlib.Path) -> pd.DataFrame:
@@ -36,18 +55,11 @@ def read_tbl(pdf_path: pathlib.Path) -> pd.DataFrame:
     p: PdfFileReader = PdfFileReader(pdf_path)
     garbage: str = '½ö¹©°²Áª×Ê²ú²Î¿¼'
     for i in range(p.getNumPages()):
-        text: list[str] = p.getPage(i).extract_text().replace(garbage, "")\
-            .strip().split("\n")
-        tbl_text: filter[str] = filter(lambda x: x.count(" ") == 6, text)
-        meta_text: filter[str] = filter(lambda x: x.count(" ") != 6, text)
-        cols: list[str] = [
-            "产品全称", "产品成立时间", "期末累计单位净值(元/份)",
-            "当月累计单位净值增长率", "年初以来累计单位净值增长率", "期末净资产(亿元)", "产品管理机构"
-        ]
-        df: pd.DataFrame = pd.DataFrame(
-            map(lambda x: x.split(" "), tbl_text), columns=cols
-        )
-        df["产品类型"] = find_prod_type("".join(meta_text))
+        txt = p.getPage(i).extract_text().replace(garbage, "").strip().split("\n")
+        normal_df = conv_normal(txt)
+        mmp_df = conv_mmp(txt)
+        df = pd.concat([normal_df, mmp_df])
+        df["产品类型"] = find_prod_type(txt)
         out.append(df)
     out2: pd.DataFrame = pd.concat(out, ignore_index=True)
     out2["产品成立时间"] = pd.to_datetime(out2["产品成立时间"], format="%Y%m%d")
@@ -61,10 +73,10 @@ def read_tbl(pdf_path: pathlib.Path) -> pd.DataFrame:
 def main() -> None:
     pdf_path: str = "/Users/shrektan/Library/CloudStorage/OneDrive-共享的库-onedrive/"\
         "安联资管文档/监管和协会资料/组合类产品信息/保险资产管理产品行业报告（2022年10月）-组合行情-开放式组合类资管产品清单.pdf"
-    out_path:str = "~/Downloads/中保登数据-2022年10月.xlsx"
+    out_path:str = "~/Downloads/中保登数据-2022年10月v2.xlsx"
     path: pathlib.Path = pathlib.Path(pdf_path)
     if not path.exists():
-        raise FileExistsError
+        raise FileExistsError(f"`{pdf_path}` doesn't exist.")
     read_tbl(path).to_excel(out_path)
 
 
