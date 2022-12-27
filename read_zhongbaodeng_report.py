a72aa5627ddd85e79b474b2cb44d262ebe84cba0
shrektan <shrektan@126.com>
2022-12-27 12:53:31 +0800

    be able to handle strange space case

--- read_zhongbaodeng_report.py
+++ read_zhongbaodeng_report.py
@@ -5,6 +5,10 @@
 import pathlib
 from PyPDF2 import PdfFileReader
 import pandas as pd
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG, force=True)
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
 
@@ -50,18 +54,52 @@ def conv_mmp(x: list[str]) -> pd.DataFrame:
     return out
 
 
+def rm_space(x: str) -> str:
+    """remove unexpected space from table strings
+
+    Args:
+        x (str): the row string read from pdf
+
+    Returns:
+        str: the cleaned row string
+    """
+    pattern = r"(.+)(?=\s\d{8})"
+    match = re.match(pattern, x)
+    if match is None:
+        return x
+    prod_name = match.group(0).replace(" ", "")
+    return re.sub(pattern, prod_name, x)
+
+
+def rm_garbage(x: str) -> list[str]:
+    """remove garbage letters
+
+    Args:
+        x (str): _description_
+
+    Returns:
+        list[str]: _description_
+    """
+    garbage = ['½ö¹©°²Áª×Ê²ú²Î¿¼', 'NÅO\x9b[\x89\x80T\x8dDN§O\x7fu(']
+    for elem in garbage:
+        x = x.replace(elem, "")
+    return x.strip().split("\n")
+
+
 def read_tbl(pdf_path: pathlib.Path) -> pd.DataFrame:
     out: list[pd.DataFrame] = []
     p: PdfFileReader = PdfFileReader(pdf_path)
-    garbage: str = '½ö¹©°²Áª×Ê²ú²Î¿¼'
     for i in range(p.getNumPages()):
-        txt = p.getPage(i).extract_text().replace(
-            garbage, "").strip().split("\n")
+        logging.info(f"handling page {i}/{p.getNumPages()}")
+        txt = list(map(rm_space, rm_garbage(p.getPage(i).extract_text())))
         normal_df = conv_normal(txt)
         mmp_df = conv_mmp(txt)
         df = pd.concat([normal_df, mmp_df])
         df["产品类型"] = find_prod_type(txt)
         out.append(df)
+        logging.debug(
+            f"page {i} has {len(df)} rows with {find_prod_type(txt)} type")
+
     out2: pd.DataFrame = pd.concat(out, ignore_index=True)
     out2["产品成立时间"] = pd.to_datetime(out2["产品成立时间"], format="%Y%m%d")
     out2["期末累计单位净值(元/份)"] = pd.to_numeric(out2["期末累计单位净值(元/份)"])
@@ -73,8 +111,8 @@ def read_tbl(pdf_path: pathlib.Path) -> pd.DataFrame:
 
 def main() -> None:
     pdf_path: str = "/Users/shrektan/Library/CloudStorage/OneDrive-共享的库-onedrive/"\
-        "安联资管文档/监管和协会资料/组合类产品信息/保险资产管理产品行业报告（2022年10月）-组合行情-开放式组合类资管产品清单.pdf"
-    out_path: str = "~/Downloads/中保登数据-2022年10月v2.xlsx"
+        "安联资管文档/监管和协会资料/组合类产品信息/保险资产管理产品行业报告（2022年11月）-组合行情-开放式组合类资管产品清单.pdf"
+    out_path: str = "~/Downloads/中保登数据-2022年11月.xlsx"
     path: pathlib.Path = pathlib.Path(pdf_path)
     if not path.exists():
         raise FileExistsError(f"`{pdf_path}` doesn't exist.")
