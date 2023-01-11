f3f325f1159b8ee0334502b2870d887f684185df
shrektan <shrektan@126.com>
2023-01-11 09:26:06 +0800

    test agains date objects

--- tests/test_writexlsx.py
+++ tests/test_writexlsx.py
@@ -1,12 +1,14 @@
 import writexlsx as w
 import pandas as pd
+from datetime import date
 
 
 def test_write(tmp_path) -> None:
     df = pd.DataFrame({
         "Col1": [1, 2.0, 3],
         "Col2": pd.Series(pd.date_range("2022-01-02", periods=3)),
-        "Col3": ["ABC", "BCD", "EFFFF"]
+        "Col3": ["ABC", "BCD", "EFFFF"],
+        "Col4": date(2022, 1, 23)
     })
 
     df2 = pd.DataFrame({
